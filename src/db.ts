c1737a1e4977f1af0fbcdebf90594c53ed48ae72
Modestas Kumpys <mode@Modestass-MacBook-Air.local>
2026-01-21 09:29:01 +0100

    chore: update db connection logic

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
 import postgres from 'postgres';
 
-const connectionString = process.env.DATABASE_URL || '';
+const connectionString = process.env.DATABASE_URL;
+// @ts-ignore: connectionString might be undefined, but we expect it to be set
 const sql = postgres(connectionString);
 
 export default sql;
